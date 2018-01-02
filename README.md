f61facfde76a6c151a668151d4919fefff15cd35
Muhammad Habib Jawady <abir.jaouadi@etudiant-fmt.utm.tn>
2018-01-02 13:20:21 +0100

    Added help

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# CTF
-Some tools for CTF
+# CTF - Tools library
+## Maintained by : [Muhammad Habib Jawady](https://twitter.com/JawadyHabib)
+
+Hello ***CTF players*** !!
+This is where you can find ready scripts for CTF solving use.
+You can import files as modules and twea your tools, and read help for additional details and use cases.
+I will be adding stuff when I find the need for when solving tasks and if you have anything to add here make sure to send me a twitter message and your name will be added to the contributors section.
+_stay tuned_
+
+Name | file
+-----|-----
+Caesar tool for enc/dec and breaking | [Caesar.py]
--- caesar.py
+++ caesar.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
 def getMode():
+    """ 
+	Collecting mode of use: (case insensitive)
+		Encrypt: 
+		  You can input encrypt or e  
+		Descrypt:
+		  You can input decrypt or d 
+	This function requires no arguments and returns the mode in type string.
+    """
     while True:
         print('Decrypt/Encrypt?')
         mode = input().lower()
@@ -10,34 +18,46 @@ def getMode():
             print('An error occured while handling your request: %s\n Enter encrypt / decrypt' % mode)
 
 def getMessage():
+    """
+	A function which requires no arguments.
+	It returns the message the user types.
+    """
     return input('Enter your message:\n')
 
 def caesar(plainText, shift):
-  cipherText = ""
-  for ch in plainText:
-    if ch.isalpha():
-      stayInAlphabet = ord(ch) + shift
-      if stayInAlphabet > ord('z'):
-        stayInAlphabet -= 26
-      finalLetter = chr(stayInAlphabet)
-      cipherText += finalLetter
-  return cipherText
-
-mode = getMode()
-message = getMessage()
-if mode[0] == 'd':
-    key = input('Please put key if you have it:')
-    if key != '':
-        print(caesar(message, int(key)))
-    else:
-        for _ in range(0,26+1):
-            print("For key %d text is %s" % (_, caesar(message, _)))
-else:
-    while True:
+    """
+	A function which takes plaintext and shift key as arguments.
+	Implementation of the Ceasar shift cipher.
+	It returns ciphertext string.
+	It can be used in the purpose of encryption or decryption.
+    """
+    cipherText = ""
+    for ch in plainText:
+      if ch.isalpha():
+        stayInAlphabet = ord(ch) + shift
+        if stayInAlphabet > ord('z'):
+          stayInAlphabet -= 26
+        finalLetter = chr(stayInAlphabet)
+        cipherText += finalLetter
+    return cipherText
+if __name__ == '__main__':
+    print('[-] Welcome to Caesar tool for CTF use')
+    print('[+] You can import this as a module and use help() for addictional data')
+    mode = getMode()
+    message = getMessage()
+    if mode[0] == 'd':
         key = input('Please put key if you have it:')
-        if key == 'quit':
-            quit()
-        elif key != '' and int(key) in range (1,27):
+        if key != '':
             print(caesar(message, int(key)))
         else:
-            print('Oops! something went wrong.')
+            for _ in range(0,26+1):
+                print("For key %d text is %s" % (_, caesar(message, _)))
+    else:
+        while True:
+            key = input('Please put key if you have it:')
+            if key == 'quit':
+                quit()
+            elif key != '' and int(key) in range (1,27):
+                print(caesar(message, int(key)))
+            else:
+                print('Oops! something went wrong.')
